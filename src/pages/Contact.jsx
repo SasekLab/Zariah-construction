1b8b66d76d7e49fe91cc6331f40fcab3cc431b45
sammydamz <hellosamdanq@gmail.com>
2026-02-09 22:09:43 +0000

    update map embed code + GPS Address

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -73,7 +73,8 @@ export default function Contact() {
                   <div className="content">
                     <h4>Our Address</h4>
                     <h6>
-                      Kokofu Mensase, Kumasi, Ghana
+                      Kokofu Mensase, Bekwai, Ghana <br />
+                      GPS ADDRESS: AB-0802-3274
                     </h6>
                   </div>
                 </div>
@@ -141,10 +142,13 @@ export default function Contact() {
           <div className="gt-map-items">
             <div className="googpemap">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6678.7619084840835!2d144.9618311901502!3d-37.81450084255415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642b4758afc1d%3A0x3119cc820fdfc62e!2sEnvato!5e0!3m2!1sen!2sbd!4v1641984054261!5m2!1sen!2sbd"
+                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2803.020667063463!2d-1.5449359340837965!3d6.512844134114323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNsKwMzAnNDcuNSJOIDHCsDMyJzM3LjEiVw!5e0!3m2!1sen!2sgh!4v1770674470474!5m2!1sen!2sgh"
+                width="600"
+                height="450"
                 style={{ border: 0 }}
                 allowFullScreen=""
                 loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
                 title="Google Map"
               ></iframe>
             </div>
